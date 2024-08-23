dc37a53282ab455f3576710f015b51ed32ff7657
geo653rge <georgekimanikiringa@gmail.com>
2024-08-23 21:01:47 +0300

    adding program section

--- eslint.config.js
+++ eslint.config.js
@@ -30,6 +30,7 @@ export default [
       ...reactHooks.configs.recommended.rules,
       "react/jsx-uses-react": "error",   
       "react/jsx-uses-vars": "error" ,
+      "react/prop-types": "off",
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
 
 const App = () => {
   return (
@@ -8,6 +10,11 @@ const App = () => {
 
 <Navbar/>
 <Hero/>
+<div className="container">
+  <Title subTitle='OUR PROGRAM' title='What We Offer'/>
+<Programs/>
+</div>
+
     </div>
   )
 }
