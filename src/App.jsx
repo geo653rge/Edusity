d950fc1c0ee4349814be88392eed62e0f241f96f
geo653rge <georgekimanikiringa@gmail.com>
2024-08-29 14:31:30 +0300

    adding testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
 
 const App = () => {
   return (
@@ -18,6 +19,8 @@ const App = () => {
 <About/>
 <Title subTitle='Gallery' title='Campus Photos'/>
 <Campus/>
+<Title subTitle='TESTIMONIALS' title='What Student says'/>
+<Testimonials/>
 </div>
 
     </div>
