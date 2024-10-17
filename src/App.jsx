71ffed18f97b1715438f04b3fc3f0f8baa725f95
geo653rge <georgekimanikiringa@gmail.com>
2024-10-17 18:39:57 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
@@ -8,8 +8,11 @@ import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testimonials/Testimonials'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
+import Video from './Components/VideoPlayer/video'
 
 const App = () => {
+
+  const[playState , setPlayState] = useState(false);
   return (
     <div>
 
@@ -18,7 +21,7 @@ const App = () => {
 <div className="container">
   <Title subTitle='OUR PROGRAM' title='What We Offer'/>
 <Programs/>
-<About/>
+<About setPlayState={setPlayState}/>
 <Title subTitle='Gallery' title='Campus Photos'/>
 <Campus/>
 <Title subTitle='TESTIMONIALS' title='What Student says'/>
@@ -27,7 +30,7 @@ const App = () => {
 <Contact/>
 <Footer/>
 </div>
-
+<Video playState={playState} setPlayState={setPlayState}/>
     </div>
   )
 }
